c36facb6a0034a6fc36594bfeeee62afe121d27a
GeekyPratham <kprathamraj2021@gmail.com>
2025-06-07 23:43:12 +0530

    update in index.ts

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,6 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 export default function handler(req: VercelRequest, res: VercelResponse) {
   res.status(200).json({
     message: 'Welcome to Resilient Email Service API!',
-    routes: ['/api/send-email', '/api/status/id']
+    routes: ['/api/send-email.ts', '/api/status/id.ts']
   });
 }
